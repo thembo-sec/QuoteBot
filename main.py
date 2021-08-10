3d36f6e7102a7e27151c3efe4f1861e3c1149e35
Alex Bek <51521812+Arbite365@users.noreply.github.com>
2021-08-10 10:16:09 +0000

    Quotes now added to database

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 import io
 
 from dotenv import load_dotenv
+from replit import db
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -13,9 +14,18 @@ client = discord.Client()
 
 bot = commands.Bot(command_prefix='!')
 
+#accepts quote and adds to replit database
+def add_quote(msg):
+  if str(msg.id) in db: #database keys must be strings for some reason
+    response = "Someone else thought it was stupid too! That quote has already been archived"
+  else:
+    db[str(msg.id)] = msg.content
+    response = "That quote is now recorded forever."
+  return response
+
+#TODO, have this add to a xlsx file. might store full message info in class
 @bot.command(name='AddQuote', help="Adds a quote to the list of cool quotes.")
-async def addQuote(ctx):  # yes, you can do msg: discord.Message
-    # but for the purposes of this, i'm using an int
+async def addQuote(ctx):  
 
     msg = ctx.message
 
@@ -24,7 +34,9 @@ async def addQuote(ctx):  # yes, you can do msg: discord.Message
         fchdMsg = await msg.channel.fetch_message(msg.reference.message_id)
         print(f' > {fchdMsg.content}')
 
-        response = "Wow! A great Quote"
+        response = "Wow! A great Quote from " + fchdMsg.author.name
+
+        await ctx.send(add_quote(fchdMsg))
 
     else:
         response = "There is no quote here"
